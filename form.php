31c22cb4168df25e612f28186bebaab8209c92a1
christian . <christianjaziel.nicolasrayon2@gmail.com>
2024-01-23 15:50:45 -0600

    eight commit

--- form.php
+++ form.php
@@ -1,17 +1,33 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["Name"];
     $email = $_POST["Email"];
-    $phone = $_POST["PhoneNumber"];
+    $phone = $_POST["PhoneN"];
     $message = $_POST["Message"];
 
-    // Aquí puedes realizar acciones como enviar un correo electrónico o guardar en una base de datos
-    // Por ejemplo, para enviar un correo electrónico de ejemplo:
-    $to = "nicolasrayon.dev@gmail.com";
-    $subject = "Nuevo formulario enviado";
-    $headers = "From: $email";
-    $mail_body = "Nombre: $name\nEmail: $email\nTeléfono: $phone\nMensaje: $message";
+    // Configurar el objeto PHPMailer
+    $mail = new PHPMailer(true);
+    try {
+        $mail->setFrom($email);
+        $mail->addAddress("nicolasrayon.dev@gmail.com");
+        $mail->Subject = "Nuevo formulario enviado";
+        $mail->Body = "Nombre: $name\nEmail: $email\nTeléfono: $phone\nMensaje: $message";
 
-    mail($to, $subject, $mail_body, $headers);
+        // Enviar el correo electrónico
+        $mail->send();
+
+        // Redirigir al usuario a index.html después de procesar el formulario
+        header("Location: index.html");
+        exit();
+    } catch (Exception $e) {
+        echo "Error al enviar el correo: {$mail->ErrorInfo}";
+    }
 }
 ?>
